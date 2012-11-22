1a81dc50b5e18a73011f44a62d33b2496ff4bb12
Michael Yin <layerssss@gmail.com>
2012-11-22 10:05:37 +0800

    remove generated files

--- publish.sh
+++ publish.sh
@@ -1,3 +1,4 @@
+
 if [ -e $GITHUB_USERNAME ] 
 then  
   echo -e "请输入你的Github用户名:\c"
@@ -7,4 +8,12 @@ then
   read GITHUB_PASSWORD
   export GITHUB_PASSWORD $GITHUB_PASSWORD
 fi
-docpad generate --out ./
+rm -R -f out
+git clone --branch master git@github.com:pansafe/pansafe.github.com.git out
+docpad generate
+cd out 
+git add --all
+git commit -m 'compiled from branch src'
+git push origin master
+cd ..
+rm -R -f out
--- scripts/script.js
+++
@@ -1,7 +0,0 @@
-(function() {
-
-  $(function() {
-    return $('#myCarousel').carousel();
-  });
-
-}).call(this);
